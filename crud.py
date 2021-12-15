59b2becbc82fc4f13e5f687cb2e796e105d7a134
Alison Cordeiro <acordeiro@gmail.com>
2021-12-15 05:58:26 -0800

    Add removal test and comments

--- crud.py
+++ crud.py
@@ -20,12 +20,29 @@ def create_student(first_name, last_name, grade):
     else:
         return 'Error: Student name already exists.' 
 
+
+def get_student_by_name(f_name, l_name):
+    """Retrieve student object by first and last name."""
+    
+    return Student.query.filter(Student.first_name.like(f_name), Student.last_name.like(l_name)).first()
+
+
+def get_all_students():
+    """Return full list of student-objects."""
+    
+    return Student.query.all()
+
+
+def get_student_by_id(id):
+    """Retrieve student-object by id."""
+
+    return Student.query.filter_by(id=id).first()
+
+
 def update_student(id, f_name, l_name, grade, lvl=None):
-    """Update a new user."""
+    """Update a user's information."""
 
     student = get_student_by_id(id)
-    print('Implement updating feature')
-
     student.first_name = f_name
     student.last_name = l_name
     student.grade = grade
@@ -35,19 +52,10 @@ def update_student(id, f_name, l_name, grade, lvl=None):
 
     return student
 
-def get_student_by_name(f_name, l_name):
-    """Retrieve student object by first and last name."""
-    
-    return Student.query.filter(Student.first_name.like(f_name), Student.last_name.like(l_name)).first()
-
-def get_all_students():
-    
-    return Student.query.all()
-
-def get_student_by_id(id):
-    return Student.query.filter_by(id=id).first()
 
 def remove_student(first_name, last_name):
+    """Remove a student object from the database."""
+    
     student = get_student_by_name(first_name, last_name) 
 
     if student:
--- test_crud.py
+++ test_crud.py
@@ -5,10 +5,6 @@ from model import db, Student, connect_to_db
 import unittest
 
 class TestStudentCreation(unittest.TestCase):
-    
-    def test_duplication(self):
-        create_student('Test', 'Student', 9)
-        self.assertEqual(create_student('Test', 'Student', 9), 'Error: Student name already exists.')
 
     def test_creation(self):
         """Confirms that total increases by 1 after new student created."""
@@ -17,22 +13,38 @@ class TestStudentCreation(unittest.TestCase):
             remove_student('Test', 'Student')
 
         num_students = len(get_all_students())
-        create_student('Test', 'Student', 18)
+        create_student('Test', 'Student', 1)
         self.assertEqual(len(get_all_students()), num_students + 1)
     
+
+    def test_duplication(self):
+        """Tests duplication error within create_student"""
+        create_student('Test', 'Student', 2)
+        self.assertEqual(create_student('Test', 'Student', 9), 'Error: Student name already exists.')
+    
+
     def test_update(self):
+        """Confirms that update_student changes grade."""
 
         if get_student_by_name('Test', 'Student'):
             remove_student('Test', 'Student')
         
-        student = create_student('Test', 'Student', 1)
+        student = create_student('Test', 'Student', 3)
         update_student(student.id, 'Test', 'Student', 99)
 
         self.assertEqual(student.grade, 99)
 
+    def test_removal(self):
+        """Confirms that remove_student deletes student object from db."""
 
-    
-        
+        if not get_student_by_name('Test', 'Student'):
+            create_student('Test', 'Student', 4)
+
+        num_students = len(get_all_students())
+        remove_student('Test', 'Student')
+        self.assertEqual(get_student_by_name('Test', 'Student'), None)
+        self.assertEqual(len(get_all_students()), num_students-1)
+
 
 
 if __name__ == '__main__':
